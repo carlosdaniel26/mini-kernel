b1a3d66bda45b77d53ce3b3ae423d4d2b67ce466
CarlosDaniel <105173667+Carlos-Daniel-Dev@users.noreply.github.com>
2024-02-12 10:36:10 -0300

    removing useless comment; now the kernel print a gun when turn on

--- kernel.c
+++ kernel.c
@@ -9,10 +9,19 @@
 void k_clear_screen();
 unsigned int k_printf(char *message, unsigned int line);
 
-/* simple kernel written in C */
 void k_main() 
 {
 	k_clear_screen();
-	k_printf("Eae linha 1", 0);
-	k_printf("Eae linha 2", 1);
+	int line = 0;
+	k_printf(" +--^----------,--------,-----,--------^-,         POW POW\n", line++);
+	k_printf(" | |||||||||   `--------'     |          O\n", line++);
+	k_printf(" `+---------------------------^----------|\n", line++);
+	k_printf("   `\\_,---------,---------,--------------'\n", line++);
+	k_printf("     / XXXXXX /'|       /'\n", line++);
+	k_printf("    / XXXXXX /  `\\    /'\n", line++);
+	k_printf("   / XXXXXX /`-------'\n", line++);
+	k_printf("  / XXXXXX /\n", line++);
+	k_printf(" / XXXXXX /\n", line++);
+	k_printf("(________(\n", line++);
+	k_printf(" `------'\n", line++);
 };
