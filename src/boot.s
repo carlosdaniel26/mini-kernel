d0cdde8fdc15e8583cec32752244bc0dd9c37b00
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2024-06-19 14:26:08 -0300

    indentation fixed in kernel.c

--- src/boot.s
+++ src/boot.s
@@ -31,10 +31,10 @@ stack is properly aligned and failure to align the stack will result in
 undefined behavior.
 */
 .section .bss
-.align 16
-stack_bottom:
-.skip 16384 # 16 KiB
-stack_top:
+	.align 16
+	stack_bottom:
+	.skip 16384 # 16 KiB
+	stack_top:
 
 /*
 The linker script specifies _start as the entry point to the kernel and the
@@ -42,8 +42,8 @@ bootloader will jump to this position once the kernel has been loaded. It
 doesn't make sense to return from this function as the bootloader is gone.
 */
 .section .text
-.global _start
-.type _start, @function
+	.global _start
+	.type _start, @function
 _start:
 	/*
 	The bootloader has loaded us into 32-bit protected mode on a x86
