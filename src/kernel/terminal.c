237ee5b9e1634340b3a4813b208822a7b2b22f28
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2024-06-21 20:35:06 -0300

    terminal color now splited in terminal color(font color) and background color, where are joined in the variable terminal_color_scheme; now the initialize function is splited to more 2 initialize functions, also the buffer is initializated in a separated function (terminal_initialize_buffer)

--- src/kernel/terminal.c
+++ src/kernel/terminal.c
@@ -9,25 +9,51 @@
 size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
+uint8_t terminal_background_color;
+uint8_t terminal_color_scheme;
 uint16_t* terminal_buffer;
 
 void terminal_initialize(void) 
 {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLUE);
-	terminal_buffer = (uint16_t*) 0xB8000;
+	terminal_set_background_color(VGA_COLOR_BLUE);
+	terminal_setcolor(VGA_COLOR_WHITE);
+
+	terminal_initialize_buffer();
+	terminal_initialize_background();
+}
+
+void terminal_initialize_background(void)
+{
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
 			const size_t index = y * VGA_WIDTH + x;
-			terminal_buffer[index] = vga_entry(' ', terminal_color);
+			terminal_buffer[index] = vga_entry(' ', terminal_color_scheme);
 		}
 	}
+}
+
+void terminal_initialize_buffer(void)
+{
+	terminal_buffer = (uint16_t*) 0xB8000;
 }
 
 void terminal_setcolor(uint8_t color) 
 {
 	terminal_color = color;
+	terminal_update_color_scheme();
+}
+
+void terminal_set_background_color(uint8_t background_color)
+{
+	terminal_background_color = background_color;
+	terminal_update_color_scheme();
+}
+
+void terminal_update_color_scheme(void)
+{
+	terminal_color_scheme = vga_entry_color(terminal_color, terminal_background_color);
 }
 
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
@@ -38,7 +64,7 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 
 void terminal_putchar(char c) 
 {
-	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+	terminal_putentryat(c, terminal_color_scheme, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
 		if (++terminal_row == VGA_HEIGHT)
@@ -46,7 +72,7 @@ void terminal_putchar(char c)
 	}
 }
 
-void terminal_breakline()
+void terminal_breakline(void)
 {
 	terminal_row++;
 	terminal_column = 0;
