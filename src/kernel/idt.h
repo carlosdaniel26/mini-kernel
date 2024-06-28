154933de98474abadffcaac64e6a2ba7f997c28a
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2024-06-28 06:54:19 -0300

    don't need the global variables of idt.h anymore

--- src/kernel/idt.h
+++ src/kernel/idt.h
@@ -10,12 +10,10 @@ struct idt_entry_struct {
     uint8_t flags;
     uint16_t base_high;
 }__attribute__((packed));
-typedef struct id_entry_sturct idt_entry_t;
 
 struct idt_ptr_struct {
     uint16_t limit;
     uint32_t base;
 } __attribute__((packed));
-typedef struct idt_ptr_struct idt_ptr_t;
 
 #endif
