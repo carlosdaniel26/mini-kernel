65296f35b672f9e5770a55f3b25be42a892c8378
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2024-07-02 21:15:02 -0300

    terminal.h now have putentryat

--- src/include/kernel/terminal.h
+++ src/include/kernel/terminal.h
@@ -12,6 +12,7 @@ void terminal_initialize_buffer(void);
 void terminal_setcolor(uint8_t color);
 void terminal_set_background_color(uint8_t background_color);
 void terminal_update_color_scheme();
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y);
 void terminal_putchar(char c);
 void terminal_breakline();
 void terminal_write(const char* data, size_t size);
