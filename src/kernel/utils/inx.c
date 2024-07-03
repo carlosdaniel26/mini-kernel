b8300175f6d2328bfe7fc224ac7941bfc231c242
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2024-07-03 16:24:04 -0300

    io_wait add

--- src/kernel/utils/inx.c
+++ src/kernel/utils/inx.c
@@ -13,6 +13,12 @@ uint8_t inb(uint16_t port)
     return ret;  // Returns the value read from the input/output port
 }
 
-void outb(uint16_t port, uint8_t val) {
+void outb(uint16_t port, uint8_t val) 
+{
     __asm__ volatile ( "outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
+}
+
+static inline void io_wait(void)
+{
+    outb(0x80, 0);
 }
