7eba718d150aa866a55d40ad53fdc9a5403b3c52
Carlos <105173667+carlosdaniel26@users.noreply.github.com>
2024-10-27 14:34:35 -0300

    improving identation

--- src/kernel/arch/i686/stack_protactor.c
+++ src/kernel/arch/i686/stack_protactor.c
@@ -1,9 +1,9 @@
 #include <stdint.h>
 
 #if UINTPTR_MAX == UINT32_MAX
-#define STACK_CHK_GUARD 0xe2dee396
+    #define STACK_CHK_GUARD 0xe2dee396
 #else
-#define STACK_CHK_GUARD 0x595e9fbd94fda766
+    #define STACK_CHK_GUARD 0x595e9fbd94fda766
 #endif
 
 uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
